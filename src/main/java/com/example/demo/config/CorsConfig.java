ea2ecba781a04e1bc1de276edf61c68f53cd485b
Sruthi3435 <rsruthi5002@gmail.com>
2026-04-11 21:55:55 +0530

    final_auth_deploy

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://sruthi3435.github.io"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
